e46a30c0c5afd0ecd072020e86d9e25c1481353e
Lyudmila <laudylim@yandex.ru>
2018-12-12 21:14:56 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function(){
+  'use strict';
+
+  let fullPageSlider = require('./parts/fullpage-slider');
+
+  fullPageSlider();
+});
---
+++ src/parts/fullpage-slider.js
@@ -0,0 +1,35 @@
+function fullPageSlider() {
+  let screen = document.querySelectorAll('.screen'),
+      next = document.querySelectorAll('.next'),
+      toFirst = document.querySelectorAll('.to-first'),
+      screenIndex = 1;
+
+  function showScreen(n) {
+    if (n > screen.length) screenIndex = 1;
+    if (n < 1) screenIndex = screen.length;
+    screen.forEach((item) => item.style.display = 'none');
+    screen[screenIndex - 1].style.display = 'block';
+  }
+
+  showScreen(screenIndex);
+
+  let nextScreen = (n) => showScreen(screenIndex += n);
+  let currentScreen = (n) => showScreen(screenIndex = n);
+
+  toFirst.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      currentScreen(1);
+    });
+  });
+
+  next.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      nextScreen(1);
+    });
+  });
+ 
+}
+
+module.exports = fullPageSlider;
